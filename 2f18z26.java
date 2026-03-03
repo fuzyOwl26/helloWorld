beae76165ef1ffafb6040fc9665df5210c320e09
fuzyOwl26 <davidjallen224@gmail.com>
2026-03-03 15:46:17 -0500

    Update 2f18z26.java

--- 2f18z26.java
+++ 2f18z26.java
@@ -1,6 +1,6 @@
 public class helloWorld {
   public static void main(String[] args) {
     String[] st = {"Hello World!", "Java is fun!", "Java programming"};
-    System.out.println(st[1]);
+    System.out.println(st[0]);
   }
 }
